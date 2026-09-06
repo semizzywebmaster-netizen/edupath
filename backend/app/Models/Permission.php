92d279209dbbd8b88175b0a038efef3b6aeb8f09
Semizzy <semizzywebmaster@gmail.com>
2026-09-06 18:32:41 +0100

    Phase 03: add Permission model

---
+++ backend/app/Models/Permission.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    protected $fillable = ['name', 'display_name', 'description'];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}
