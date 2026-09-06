aca67be6498e5abba570a592d4ac76df983f1494
Semizzy <semizzywebmaster@gmail.com>
2026-09-06 18:32:36 +0100

    Phase 03: add Role model

---
+++ backend/app/Models/Role.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $fillable = ['name', 'display_name', 'description'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+}
